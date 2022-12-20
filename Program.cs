7d6b7ff411a0aec94dea2fa83db62691749bfdad
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-12-20 18:21:21 -0400

    feat: function and tests

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace PercentageOfVolume
+﻿using System.Globalization;
+
+namespace PercentageOfVolume
 {
-    internal class Program
+    public class Program
     {
+        public static int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado)
+        {
+            decimal porcentagemDoTotal = 1.0M - porcentagem;
+            int pov = (int)Math.Truncate(((totalNegociado * porcentagem) / porcentagemDoTotal));
+
+            return pov;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] totaisNegociados = new int[] { 90, 100, 70, 81 };
+
+            decimal[] porcentagens = new decimal[] { 0.1M, 0.1M, 0.2M, 0.4M };
+
+            int[] povs = new int[] { 10, 11, 17, 54 };
+
+            for (var teste = 0; teste < totaisNegociados.Length; teste++)
+            {
+                int povAtual = funcaoRetornaQuantitadePOV(porcentagens[teste], totaisNegociados[teste]);
+                if (povAtual == povs[teste])
+                {
+                    Console.WriteLine($"Teste {teste + 1} - PASS! - ({porcentagens[teste].ToString(new CultureInfo("en-US"))}, {totaisNegociados[teste]}) => {povs[teste]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Teste {teste + 1} - FAIL! - ({porcentagens[teste].ToString(new CultureInfo("en-US"))}, {totaisNegociados[teste]}) => Expected: {povs[teste]}, Got: {povAtual}");
+                }
+            }
         }
     }
 }
